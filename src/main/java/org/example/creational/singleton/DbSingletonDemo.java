e280509778cb4c54f7c686496da870dcd73b22cf
lelis <lelis685@gmail.com>
2023-01-07 21:03:15 -0300

    config inicial

---
+++ src/main/java/org/example/creational/singleton/DbSingletonDemo.java
@@ -0,0 +1,18 @@
+package org.example.creational.singleton;
+
+public class DbSingletonDemo {
+
+    public static void main(String[] args) {
+
+        DbSingleton instance = DbSingleton.getInstance();
+        DbSingleton instance2 = DbSingleton.getInstance();
+
+        System.out.println(instance);
+        System.out.println(instance2);
+
+        System.out.println(instance == instance2);
+
+
+    }
+
+}
