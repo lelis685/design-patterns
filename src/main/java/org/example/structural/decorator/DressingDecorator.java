41609d63e05eb4b0e0a7398c62cfa0a22ca66cf5
lelis <lelis685@gmail.com>
2023-01-10 08:46:27 -0300

    decorator pattern

---
+++ src/main/java/org/example/structural/decorator/DressingDecorator.java
@@ -0,0 +1,17 @@
+package org.example.structural.decorator;
+
+public class DressingDecorator extends SandwichDecorator{
+
+    public DressingDecorator(Sandwich sandwich) {
+        super(sandwich);
+    }
+
+    public String make() {
+        return sandwich.make() + addDressing();
+    }
+
+    private String addDressing() {
+        return " + mustard";
+    }
+
+}
