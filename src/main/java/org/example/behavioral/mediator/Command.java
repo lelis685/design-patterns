0e1ce9ed86cbc24a1c05dde47039ddfe4a679852
lelis <lelis685@gmail.com>
2023-01-14 17:54:14 -0300

    mediator pattern

---
+++ src/main/java/org/example/behavioral/mediator/Command.java
@@ -0,0 +1,6 @@
+package org.example.behavioral.mediator;
+
+//colleague
+public interface Command {
+	void execute();
+}
---
+++ src/main/java/org/example/behavioral/mediator/TurnOnAllLightsCommand.java
@@ -0,0 +1,16 @@
+package org.example.behavioral.mediator;
+
+//concrete command
+public class TurnOnAllLightsCommand implements Command {
+
+	private Mediator med;
+	
+	public TurnOnAllLightsCommand(Mediator med) {
+		this.med = med;
+	}
+	
+	@Override
+	public void execute() {
+		med.turnOnAllLights();
+	}
+}
